28ac68fc272e65a323a5db15d0af22558109bba0
Ed Sadler <ed_lorie@comcast.net>
2016-08-03 15:25:12 -0500

    bunch o styling

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ for (i = 0; i < songs.length; i++) {
     var replaceGreaterThan = removeThings.replace(">", "-");
 
   finalArray.push(replaceGreaterThan)
+  var songName = replaceGreaterThan.slice(0, replaceGreaterThan.indexOf(" -"));
+    var artistName = replaceGreaterThan.slice(replaceGreaterThan.indexOf("by")+3, replaceGreaterThan.indexOf("on the album"));
+    var albumName = replaceGreaterThan.slice(replaceGreaterThan.indexOf("album")+6);
+console.log("song",songName);
+console.log("artist",artistName);
+console.log("album",albumName);
 
-  finalSongs.innerHTML += `<p> ${finalArray[i]}</p>`
+
+  finalSongs.innerHTML += `
+  <div>
+    <h2>${songName}</h2>
+    <ol>
+      <li>${artistName}</li>
+      <li>|</li>
+      <li>${albumName}</li>
+      <li>|</li>
+      <li>Genre</li>
+    </ol>
+  </div>`
 }
 console.log(finalArray);
 
